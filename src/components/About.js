9c029253bc2d69a307fd708390b2f40cae148657
João Pedro Rosada Volponi <jpvrosada@hotmail.com>
2023-07-13 13:16:41 -0300

    Criando Services

--- src/components/About.js
+++ src/components/About.js
@@ -23,19 +23,19 @@ const About = () => {
         <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
           { /* Img */}
           <motion.div
-          variants={fadeIn('right', 0.3)}
-          initial="hidden"
-          whileInView={"show"}
-          viewport={{once: false, amount: 0.3}}
-          className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'>
+            variants={fadeIn('right', 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'>
           </motion.div>
           { /* text */}
-          <motion.div 
-          variants={fadeIn('left', 0.5)}
-          initial="hidden"
-          whileInView={"show"}
-          viewport={{once: false, amount: 0.3}}
-          className='flex-1'>
+          <motion.div
+            variants={fadeIn('left', 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1'>
             <h2 className='h2 text-accent'> About me.</h2>
             <h3 className='h3 mb-4'>i`m a Freelancer Front-end Developer with over 5 years of
               experience
@@ -50,7 +50,7 @@ const About = () => {
             <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
               <div>
                 <div className='text-[40px] font-tertiary text-gradient mb-2'>
-                  {inView ? <CountUp start={0} end={15} durattion={3} /> : null}
+                  {inView ? <CountUp start={0} end={150} durattion={5} /> : null}
                   k+
                 </div>
                 <div className='font-primary text-sm tracking-[2px]'>
@@ -60,7 +60,7 @@ const About = () => {
               </div>
               <div>
                 <div className='text-[40px] font-tertiary text-gradient mb-2'>
-                  {inView ? <CountUp start={0} end={13} durattion={3} /> : null}
+                  {inView ? <CountUp start={0} end={100} durattion={3} /> : null}
                 </div>
                 <div className='font-primary text-sm tracking-[2px]'>
                   Projects <br />
@@ -83,7 +83,7 @@ const About = () => {
               <a href='#' className='text-gradient btn-link'>
                 My Portfolio
               </a>
-              
+
             </div>
           </motion.div>
         </div>
