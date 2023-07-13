9bab48a386baad8223aed39c712d0ade5c26b22d
João Pedro Rosada Volponi <jpvrosada@hotmail.com>
2023-07-13 15:43:07 -0300

    Finalizando Contact

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -17,8 +17,8 @@ import{fadeIn} from '../variants'
 
 const Banner = () => {
   return (
-    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
-      <div className='container mx-auto'>
+    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home' >
+      <div className='container mx-auto' >
         <div className='flex flex-col gapy-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
 
           {/* Text */ }
